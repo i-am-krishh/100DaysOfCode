84a9a780e966d8e4a3ba79bc3932b0117d857277
Krushna Wagh <wagh.krushna2004@gmail.com>
2024-06-28 10:00:42 +0530

    basics

---
+++ Python/day 1/day1.py
@@ -0,0 +1,37 @@
+# basic syntax of python code 
+print("Hello, world!")
+
+# variables 
+# Variables are containers for storing data values.
+# Variables declaration
+x = 10
+y = "This is a string"
+
+# comments
+# "This is a single line comment"
+"""This is a multiline comment
+    Multiline comment continues here"""
+
+# printing variables
+print("value of the x is: ",x)
+print("value of the y is: ",y)
+
+# Typecasting
+a = int('55')
+b = float(55)
+c = int(a)
+# print(c)
+print("Type of a is ",type(a))
+print("Type of b is ",type(b))
+print("Type of c is ",type(c))
+
+
+# Challenge 
+'''print the type of the variable'''
+
+# solution
+# print("Type of b is ",(type(b).__name__))
+
+if 5 > 2 :
+    print("5 is greater than 2")
+
